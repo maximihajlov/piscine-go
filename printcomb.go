ef94252ab1a7341efa39f652bae1b79387942f4a
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 11:08:31 +0300

    printcomb is a package piscine now

--- printcomb.go
+++ printcomb.go
@@ -1,8 +1,8 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
-func main() {
+func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := i + 1; j <= '9'; j++ {
 			for k := j + 1; k <= '9'; k++ {
