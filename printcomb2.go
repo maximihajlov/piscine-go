907e28897c2839d2fec9106d9b80f634096d1ea8
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 11:26:32 +0300

    A little optimization of printcombw

--- printcomb2.go
+++ printcomb2.go
@@ -7,16 +7,14 @@ func PrintComb2() {
 		for j := '0'; j <= '9'; j++ {
 			for k := i; k <= '9'; k++ {
 				for m := j + 1; m <= '9'; m++ {
-					if string(i)+string(j) != string(k)+string(m) {
-						z01.PrintRune(i)
-						z01.PrintRune(j)
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(m)
+					if string(i)+string(j)+string(k)+string(m) != "9899" {
+						z01.PrintRune(',')
 						z01.PrintRune(' ')
-						z01.PrintRune(k)
-						z01.PrintRune(m)
-						if string(i)+string(j)+string(k)+string(m) != "9899" {
-							z01.PrintRune(',')
-							z01.PrintRune(' ')
-						}
 					}
 				}
 			}
