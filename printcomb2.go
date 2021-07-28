425628736a781f7e9be9846051237798a64c6a8e
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 11:30:49 +0300

    last correct version of printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -6,15 +6,17 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := i; k <= '9'; k++ {
-				for m := j + 1; m <= '9'; m++ {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(k)
-					z01.PrintRune(m)
-					if string(i)+string(j)+string(k)+string(m) != "9899" {
-						z01.PrintRune(',')
+				for m := j; m <= '9'; m++ {
+					if string(i)+string(j) != string(k)+string(m) {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
 						z01.PrintRune(' ')
+						z01.PrintRune(k)
+						z01.PrintRune(m)
+						if string(i)+string(j)+string(k)+string(m) != "9899" {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
 					}
 				}
 			}
