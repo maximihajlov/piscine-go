23c88db27a7c8e021f27c6014cd435c8c259ee6e
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 13:06:52 +0300

    last printnbr

--- printnbr.go
+++ printnbr.go
@@ -5,24 +5,27 @@ import (
 )
 
 func PrintNbr(n int) {
-	var tmpn int = n
-
-	if tmpn < 0 {
+	if n < 0 {
 		z01.PrintRune('-')
-		tmpn *= -1
-	}
-	var s [19]int8
-
-	for i := 0; i < 19; i++ {
-		if tmpn == 0 {
-			s[18-i] = -1
+		if n == -9223372036854775808 {
+			z01.PrintRune('9')
+			n = 223372036854775808
 		} else {
-			s[18-i] = int8(tmpn % 10)
-			tmpn /= 10
+			n *= -1
+		}
+	}
+	var s [20]int8
+
+	for i := 0; i < 20; i++ {
+		if n == 0 {
+			s[19-i] = -1
+		} else {
+			s[19-i] = int8(n % 10)
+			n /= 10
 		}
 	}
 
-	for i := 0; i < 19; i++ {
+	for i := 0; i < 20; i++ {
 		if s[i] != -1 {
 			for l := '0'; l <= '9'; l++ {
 				if s[i] == int8(int(l)-48) {
@@ -31,5 +34,4 @@ func PrintNbr(n int) {
 			}
 		}
 	}
-	z01.PrintRune('\n')
 }
