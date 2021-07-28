466d8103b8fcf015b786c9977d2ad9124f8e9a88
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 10:21:08 +0300

    Your z01 doesn't work without go.mod file or changing GO111MODULE to auto.

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,27 +1,10 @@
 package main
 
-import (
-	"errors"
-	"os"
-	"unicode/utf8"
-)
-
-// PrintRune prints a single rune (Unicode code point) and returns any error
-// if the encoding or the writing fails.
-func PrintRune(r rune) error {
-	l := utf8.RuneLen(r)
-	if l == -1 {
-		return errors.New("The rune is not a valid value to encode in UTF-8")
-	}
-	p := make([]byte, l)
-	utf8.EncodeRune(p, r)
-	_, err := os.Stdout.Write(p)
-	return err
-}
+import "github.com/01-edu/z01/"
 
 func main() {
 	for l := 'a'; l < 'z'; l++ {
-		PrintRune(l)
+		z01.PrintRune(l)
 	}
-	PrintRune('\n')
+	z01.PrintRune('\n')
 }
