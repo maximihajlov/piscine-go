315e80c91b3aae49fd6f9d128987f88f437a6ac1
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-28 11:43:20 +0300

    printcomb last

--- printcomb2.go
+++ printcomb2.go
@@ -6,7 +6,13 @@ func PrintComb2() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
 			for k := i; k <= '9'; k++ {
-				for m := j; m <= '9'; m++ {
+				var tmp rune
+				if int(k) > int(i) {
+					tmp = '0'
+				} else {
+					tmp = j
+				}
+				for m := tmp; m <= '9'; m++ {
 					if string(i)+string(j) != string(k)+string(m) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
