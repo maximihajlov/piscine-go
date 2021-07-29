70be95ef1505ab134af2cc54936f244813cfcfbf
Maksim Mikhailov <maximihajlov@gmail.com>
2021-07-29 10:45:05 +0300

    atoi is done

---
+++ atoi.go
@@ -0,0 +1,25 @@
+package piscine
+
+func Atoi(s string) int {
+	sArr := []rune(s)
+	minus := 1
+	if sArr[0] == '-' {
+		minus = -1
+		sArr[0] = '0'
+	} else if sArr[0] == '+' {
+		sArr[0] = '0'
+	}
+	n := 0
+	for i := 0; i < len(sArr); i++ {
+		if sArr[i] > 57 || sArr[i] < 48 {
+			return 0
+		}
+		pow := 1
+		for j := 1; j < len(sArr)-i; j++ {
+			pow *= 10
+		}
+
+		n += int(sArr[i]-48) * pow
+	}
+	return minus * n
+}
