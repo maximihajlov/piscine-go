440a40670085786eabbfa563b09ef4b9222e08bf
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-02 09:37:26 +0300

    Q4 - factorial - forgot about nb<0

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,9 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	ans := 1
-	for i := 1; i <= nb; i++ {
-		ans *= i
+	if nb < 0 {
+		return 0
+	} else {
+		ans := 1
+		for i := 1; i <= nb; i++ {
+			ans *= i
+		}
+		return ans
 	}
-	return ans
 }
