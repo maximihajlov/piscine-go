9c50c2836b1d9b4b487352b35a3bea880dc6286b
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-02 09:59:36 +0300

    Q4 - recursivepower - fixed (0,0) and (n,0)

--- recursivepower.go
+++ recursivepower.go
@@ -4,8 +4,8 @@ func RecursivePower(nb int, power int) int {
 	if power < 0 {
 		return 0
 	}
-	if power == 1 {
-		return nb
+	if power == 0 {
+		return 1
 	}
 	return nb * RecursivePower(nb, power-1)
 }
