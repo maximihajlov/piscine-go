27748c60218334f07352a11b42d173fc138df103
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-03 11:42:28 +0300

    Q5 - capitalize - fixed 9wZDXKG91J`W- situation

--- capitalize.go
+++ capitalize.go
@@ -6,8 +6,10 @@ func Capitalize(s string) string {
 		sArr[0] += 'A' - 'a'
 	}
 	for i := 1; i < len(sArr); i++ {
-		if !('0' <= sArr[i-1] && sArr[i-1] <= '9' || 'a' <= sArr[i-1] && sArr[i-1] <= 'z' || 'A' <= sArr[i-1] && sArr[i-1] <= 'Z') && 'a' <= sArr[i] && sArr[i] <= 'z' {
-			sArr[i] += 'A' - 'a'
+		if !('0' <= sArr[i-1] && sArr[i-1] <= '9' || 'a' <= sArr[i-1] && sArr[i-1] <= 'z' || 'A' <= sArr[i-1] && sArr[i-1] <= 'Z') {
+			if 'a' <= sArr[i] && sArr[i] <= 'z' {
+				sArr[i] += 'A' - 'a'
+			}
 		} else if 'A' <= sArr[i] && sArr[i] <= 'Z' {
 			sArr[i] -= 'A' - 'a'
 		}
