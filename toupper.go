2f5a47cfb42fc1ec269d490a6f7a27aeb1348ae6
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-03 10:33:28 +0300

    Q5 - toupper - done

---
+++ toupper.go
@@ -0,0 +1,11 @@
+package piscine
+
+func ToUpper(s string) string {
+	sArr := []rune(s)
+	for i := range sArr {
+		if 'a' <= sArr[i] && sArr[i] <= 'z' {
+			sArr[i] -= 'a' - 'A'
+		}
+	}
+	return string(sArr)
+}
