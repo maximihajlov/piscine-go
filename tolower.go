d7081f649d921f971ca1c7a6d0af4c75fb56ce83
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-03 10:34:56 +0300

    Q5 - tolower - done

---
+++ tolower.go
@@ -0,0 +1,11 @@
+package piscine
+
+func ToLower(s string) string {
+	sArr := []rune(s)
+	for i := range sArr {
+		if 'A' <= sArr[i] && sArr[i] <= 'Z' {
+			sArr[i] += 'a' - 'A'
+		}
+	}
+	return string(sArr)
+}
