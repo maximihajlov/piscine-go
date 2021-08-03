220738602318fbdae625c8b1adc8d9c89a3ce726
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-03 11:20:33 +0300

    Q5 - trimatoi - done

---
+++ trimatoi.go
@@ -0,0 +1,19 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	sArr := []rune(s)
+	ans := 0
+	minusWas := false
+	for i := range sArr {
+		if '0' <= sArr[i] && sArr[i] <= '9' {
+			ans *= 10
+			ans += int(sArr[i] - '0')
+		} else if sArr[i] == '-' && !minusWas && ans == 0 {
+			minusWas = true
+		}
+	}
+	if minusWas {
+		ans *= -1
+	}
+	return ans
+}
