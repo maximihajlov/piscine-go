1f882e7e3d0eabe99c8c157d81b06897327688d6
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-04 10:15:43 +0300

    Q6 - sortparams - done

---
+++ sortparams/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for i := range args {
+		for j := i; j < len(args); j++ {
+			if args[i] > args[j] {
+				args[i], args[j] = args[j], args[i]
+			}
+		}
+	}
+	for i := 1; i < len(args); i++ {
+		arg := []rune(args[i])
+		for j := range arg {
+			z01.PrintRune(arg[j])
+		}
+		z01.PrintRune('\n')
+	}
+}
