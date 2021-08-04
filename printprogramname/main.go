017b223756c136b0d45fc8a9110f36cdf8173db3
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-04 09:57:51 +0300

    Q6 - printprogramme - added main.go

---
+++ printprogramname/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	name := os.Args[0]
+	nArr := []rune(name)
+	for i := len(nArr) - 1; i >= 0; i-- {
+		if nArr[i] == '/' {
+			for j := i + 1; j < len(nArr); j++ {
+				z01.PrintRune(nArr[j])
+			}
+		}
+	}
+}
