b1c132e6a797258c23376e500d67f9adbf02b3ad
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-04 10:20:28 +0300

    Q6 - sortparams - forgot about name in args[0] and printing that

--- sortparams/main.go
+++ sortparams/main.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	args := os.Args
 	for i := 1; i < len(args); i++ {
-		for j := i; j < len(args); j++ {
+		for j := i + 1; j < len(args); j++ {
 			if args[i] > args[j] {
 				args[i], args[j] = args[j], args[i]
 			}
 		}
 	}
-	for i := 0; i < len(args); i++ {
+	for i := 1; i < len(args); i++ {
 		arg := []rune(args[i])
 		for j := range arg {
 			z01.PrintRune(arg[j])
