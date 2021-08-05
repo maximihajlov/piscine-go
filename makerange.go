388b87ef905249042536fcd3c2f5c2a75738cd9a
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-05 10:01:56 +0300

    Q6 - makerange - fixed len out of range

--- makerange.go
+++ makerange.go
@@ -2,10 +2,10 @@ package piscine
 
 func MakeRange(min, max int) []int {
 	var ans []int
-	if max < min {
+	if max <= min {
 		return ans
 	}
-	ans = make([]int, min, max)
+	ans = make([]int, max-min)
 	for i := range ans {
 		ans[i] = min + i
 	}
