e9deb751f8aad35b54243fc849fc05a02799cf73
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-09 10:09:43 +0300

    Q7 - displayfile - done

---
+++ displayfile/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("File name missing")
+		return
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+		return
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(data))
+}
