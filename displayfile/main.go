e3e3b329b1289c80e451d6c6c067b973e92b2772
Maksim Mikhailov <maximihajlov@gmail.com>
2021-08-09 10:10:48 +0300

    Q7 - displayfile - all \n removed

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,16 +8,16 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("File name missing")
+		fmt.Print("File name missing")
 		return
 	} else if len(os.Args) > 2 {
-		fmt.Println("Too many arguments")
+		fmt.Print("Too many arguments")
 		return
 	}
 	data, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Println(err)
+		fmt.Print(err)
 		return
 	}
-	fmt.Println(string(data))
+	fmt.Print(string(data))
 }
